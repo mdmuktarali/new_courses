9649217461e3d1f11be424a47cbf1ca979b9aab8
Muktar <mdmuktarali100@gmail.com>
2017-04-06 16:49:44 +0600

    New Uploaded

--- README.md
+++
@@ -1,2 +0,0 @@
-# new_courses
-New Courses Design
---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        autoplay: true,
+    });
+});
+
+/*Mix it up*/
+$('').mixItUp({
+    animation: {
+        effects: 'fade rotateZ(-180deg)',
+        duration: 700
+    },
+
+});
+
+/*Select Two For Register*/
+
+
+
+
+
